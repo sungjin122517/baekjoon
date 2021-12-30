b8d91ba11dc8d6056066afe197662401b95f0781
sungjin122517 <sungjin122517@naver.com>
2021-12-30 16:57:56 +0800

    update no.2739: multiplication table

---
+++ C++ 배우기/2739.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n>=1 && n<=9)
+        for (int i=1; i<10; i++)
+            cout << n << " * " << i << " = " << n*i << "\n";
+
+    return 0;
+}
