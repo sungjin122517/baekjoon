d4651c21cbd136fe42a1724fa28076701ef5da2c
sungjin122517 <sungjin122517@naver.com>
2022-01-02 16:50:40 +0800

    no.10952: A+B 2

---
+++ C++ 배우기/10952.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    while (true) {
+        cin >> a >> b;
+        if (a+b == 0)
+            break;
+        else
+            cout << a+b << '\n';
+    }
+
+    return 0;
+}
